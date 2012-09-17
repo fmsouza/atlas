555f3e7c5b3131c13b91f6ef0bf8f3b480c19cfd
Julio Cesar <julio-cesar@ufrj.br>
2012-09-17 00:06:55 -0300

    Adicionado metodos estaticos para a classe de configuracao

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,18 @@
 <?php
-/* if(!class_exists('Config')):
+if(!class_exists('Config')):
 
-class config{
-	public String $base_url = 'http://'.$_SERVER['SERVER_NAME'].'/';
-	public String $charset	= 'UTF-8';
+class Config{
+
+	static public function charset(){
+		return "UTF-8";
+	}
+	
+	static public function base_url(){
+		return "http://{$_SERVER['SERVER_NAME']}/";
+	}
 }
 
 endif;
-*/
 
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/';
-$config['charset']  = 'UTF-8';
+//$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/';
+//$config['charset']  = 'UTF-8';
