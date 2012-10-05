2da7a35d08fe7e7f8c70a107dbd3513805fa21b4
Fred Souza <fmsouza@cisi.coppe.ufrj.br>
2012-10-05 10:55:05 -0300

    Documentação;

--- application/environment/config.php
+++ application/environment/config.php
@@ -1,9 +1,20 @@
 <?php
     /**
-     * Classe que armazena os dados do ambiente
+     * 
+     * Classe Config
+     * 
+     * Classe que armazena as configurações do ambiente
+     * 
+     * @author Frederico Souza (fmsouza@cisi.coppe.ufrj.br)
+     * 
+     * @static @param string $main_controller
+     * @static @param string $main_method
+     * 
      */
     
     class Config{
         public static $main_controller = 'Main';
         public static $main_method = 'index';
     }
+
+// Fim do arquivo Config.php
