7b33f59997c5c1067ed0d13d312fcda01e58d76c
Fred Souza <fredericoamsouza@gmail.com>
2015-02-15 23:26:08 -0200

    Correcting typo in documentation

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Atlas
  * 
- * @copyright Copyright 2013 Marvie
+ * @copyright Copyright 2013 Atlas
  * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
